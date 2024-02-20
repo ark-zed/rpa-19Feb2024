6210d2b9415dbbb4036163128970252792be0496
ark-zed <160452161+ark-zed@users.noreply.github.com>
2024-02-20 14:23:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ app=Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
   return(render_template("index.html"))
+@app.route("/main",methods=["GET","POST"])
+def main():
+  name = request.force.get("name")
+  return(render_temmplate("main.html",r=name))
 if __name__ == "__main__":
   app.run()
