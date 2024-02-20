010e5f1875ff4c99e61c3004a69823343583ceb0
ark-zed <160452161+ark-zed@users.noreply.github.com>
2024-02-20 14:46:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def index():
   return(render_template("index.html"))
 @app.route("/main",methods=["GET","POST"])
 def main():
-  name = request.force.get("name")
-  return(render_temmplate("main.html",r=name))
+  name = request.force.get("r")
+  return(render_template("main.html",r=r))
 if __name__ == "__main__":
   app.run()
